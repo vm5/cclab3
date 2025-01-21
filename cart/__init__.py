53871d950d42a81693c68d8b50609968f8632107
vedant PES2UG22CS653 <62893030+vm5@users.noreply.github.com>
2025-01-21 16:25:42 +0530

    Add files via upload

---
+++ cart/__init__.py
@@ -0,0 +1,50 @@
+import json
+
+import products
+from cart import dao
+from products import Product
+
+
+class Cart:
+    def __init__(self, id: int, username: str, contents: list[Product], cost: float):
+        self.id = id
+        self.username = username
+        self.contents = contents
+        self.cost = cost
+
+    def load(data):
+        return Cart(data['id'], data['username'], data['contents'], data['cost'])
+
+
+def get_cart(username: str) -> list:
+    cart_details = dao.get_cart(username)
+    if cart_details is None:
+        return []
+    
+    items = []
+    for cart_detail in cart_details:
+        contents = cart_detail['contents']
+        evaluated_contents = eval(contents)  
+        for content in evaluated_contents:
+            items.append(content)
+    
+    i2 = []
+    for i in items:
+        temp_product = products.get_product(i)
+        i2.append(temp_product)
+    return i2
+
+    
+
+
+def add_to_cart(username: str, product_id: int):
+    dao.add_to_cart(username, product_id)
+
+
+def remove_from_cart(username: str, product_id: int):
+    dao.remove_from_cart(username, product_id)
+
+def delete_cart(username: str):
+    dao.delete_cart(username)
+
+
